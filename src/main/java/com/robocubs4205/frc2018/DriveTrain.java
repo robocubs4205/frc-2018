1b9974c7c550566f71ef1277f73803d826b1ae9b
Trevor Giddings <tgiddings000@gmail.com>
2018-01-23 19:57:51 -0800

    Add DriveEncoder command to DriveTrain Drives a specified distance

--- src/main/java/com/robocubs4205/frc2018/DriveTrain.java
+++ src/main/java/com/robocubs4205/frc2018/DriveTrain.java
@@ -2,6 +2,8 @@ package com.robocubs4205.frc2018;
 
 import com.ctre.phoenix.drive.DriveMode;
 import com.ctre.phoenix.drive.MecanumDrive;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.InstantCommand;
@@ -9,18 +11,25 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 
 @SuppressWarnings("FieldCanBeLocal")
 class DriveTrain extends Subsystem {
-    private final MecanumDrive drive;
+    private TalonSRX frontLeft = new TalonSRX(13);
+    private TalonSRX frontRight = new TalonSRX(11);
+    private TalonSRX rearLeft = new TalonSRX(12);
+    private TalonSRX rearRight = new TalonSRX(10);
+
+    private final int CPR = 4096;
+
+    private final double wheelDiameter = 0.5;
+    private final double wheelCircumference = wheelDiameter*Math.PI;
+    private final int CPF = (int) (CPR/wheelCircumference);
+
+    private final MecanumDrive drive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
 
     private final double proportionalLateralSpeed = 1;
     private final double proportionalTurnSpeed = 1;
 
     DriveTrain() {
-        TalonSRX frontLeft = new TalonSRX(13);
-        TalonSRX frontRight = new TalonSRX(11);
-        TalonSRX rearLeft = new TalonSRX(12);
-        TalonSRX rearRight = new TalonSRX(10);
-
-        drive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
+        rearLeft.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+        rearRight.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
     }
 
     @Override
@@ -67,6 +76,50 @@ class DriveTrain extends Subsystem {
         @Override
         protected boolean isFinished() {
             return false;
+        }
+    }
+
+    class DriveEncoder extends PerpetualCommand {
+        private final double distance;
+        private final double speed;
+
+        /**
+         * Drive forward a specific distance
+         * @param distance the distance in feet
+         */
+        DriveEncoder(double distance){
+            this(distance,0.5);
+        }
+
+        /**
+         * Drive forward a specific distance
+         * @param distance the distance in feet
+         * @param speed the speed on the range (0,1]
+         */
+        DriveEncoder(double distance, double speed){
+            this.distance = distance;
+            this.speed = speed;
+        }
+
+        @Override
+        protected void initialize(){
+            rearLeft.setSelectedSensorPosition(0,0,10);
+            rearRight.setSelectedSensorPosition(0,0,10);
+            rearLeft.config_kP(0,0.125,10);
+            rearRight.config_kP(0,0.125,10);
+            rearLeft.configPeakOutputForward(speed,10);
+            rearRight.configPeakOutputForward(speed,10);
+            rearLeft.configPeakOutputReverse(-speed,10);
+            rearRight.configPeakOutputReverse(-speed,10);
+
+            frontLeft.follow(rearLeft);
+            frontRight.follow(rearRight);
+        }
+
+        @Override
+        protected void execute(){
+            rearLeft.set(ControlMode.Position,CPF*distance);
+            rearRight.set(ControlMode.Position,CPF*distance);
         }
     }
 }
