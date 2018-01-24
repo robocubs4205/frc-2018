51367d57c2007cd97be36fd6f1d6fc7b05ec572f
Trevor Giddings <tgiddings000@gmail.com>
2018-01-23 19:55:31 -0800

    Create PerpetualCommand class which never finishes The opposite of InstantCommand

---
+++ src/main/java/com/robocubs4205/frc2018/PerpetualCommand.java
@@ -0,0 +1,10 @@
+package com.robocubs4205.frc2018;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+class PerpetualCommand extends Command{
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+}
