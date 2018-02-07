0aa61be03245d8b45b515d89bed67d214f6afdc0
Trevor Giddings <tgiddings000@gmail.com>
2018-02-06 18:55:40 -0800

    Make PerpetualCommand abstract

--- src/main/java/com/robocubs4205/frc2018/PerpetualCommand.java
+++ src/main/java/com/robocubs4205/frc2018/PerpetualCommand.java
@@ -2,7 +2,7 @@ package com.robocubs4205.frc2018;
 
 import edu.wpi.first.wpilibj.command.Command;
 
-class PerpetualCommand extends Command{
+abstract class PerpetualCommand extends Command{
     @Override
     protected boolean isFinished() {
         return false;
