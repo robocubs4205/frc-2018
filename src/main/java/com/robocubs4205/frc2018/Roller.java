39d2c43dbce6cf11612090f5cff56f644fb6736d
Trevor <tgiddings000@gmail.com>
2018-02-10 11:33:43 -0800

    Create Roller Subsystem

---
+++ src/main/java/com/robocubs4205/frc2018/Roller.java
@@ -0,0 +1,51 @@
+package com.robocubs4205.frc2018;
+
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.command.InstantCommand;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Roller extends Subsystem{
+    private final Spark motor = new Spark(7);
+
+    private final double outSpeed = 1;
+    private final double inSpeed = 1;
+
+    {
+        motor.setInverted(true);
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+        setDefaultCommand(new Stop());
+    }
+
+    class Stop extends InstantCommand{
+        Stop(){
+            requires(Roller.this);
+        }
+        @Override
+        protected void initialize(){
+            motor.set(0);
+        }
+    }
+
+    class Out extends PerpetualCommand{
+        Out(){
+            requires(Roller.this);
+        }
+        @Override
+        protected void execute(){
+            motor.set(outSpeed);
+        }
+    }
+
+    class In extends PerpetualCommand{
+        In(){
+            requires(Roller.this);
+        }
+        @Override
+        protected void execute(){
+            motor.set(inSpeed);
+        }
+    }
+}
